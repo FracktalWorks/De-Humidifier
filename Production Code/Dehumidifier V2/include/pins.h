423226aaf5fb02eab55633bd7badbfcbebb13090
Vijay Raghav Varada <vjvarada@users.noreply.github.com>
2024-03-15 08:47:28 +0530

    Created Production Folder, organized files for VS code/ platform.io compilation

---
+++ Production Code/Dehumidifier V2/include/pins.h
@@ -0,0 +1,25 @@
+
+
+                  //ESP32 S3    //ESP WROOM 32 DEVKIT 
+#define NEOPIXEL_PIN  14            //19
+#define CLOCK         36            //14
+#define DATA          35            //13
+#define CS            21            //16
+#define DC            47            //17
+#define RESET         13            //4
+
+#define DHT_PIN       12            //2
+
+#define EN_A          39            //27
+#define EN_B          40            //26
+#define EN_BTN        1//45            //5
+
+#define BUZZER        48            //18
+
+#define RELAY_1       15            //21
+#define RELAY_2       16            //22
+
+//#define DT_A          23
+//#define SCK_A         32
+//#define DT_B          25
+//#define SCK_B         33
